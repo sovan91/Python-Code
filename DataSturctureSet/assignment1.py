ee5de32bb9e357957bc5461924d3ff5a554eab38
Sovandev Chakrabarty <42212491+sovan91@users.noreply.github.com>
2019-02-08 18:53:24 +0530

    Add files via upload

---
+++ DataSturctureSet/assignment1.py
@@ -0,0 +1,12 @@
+def duplicate():
+  
+  s = set()
+  for i in (words):
+      words = input('Enter any words:')
+    
+      s.add(words)
+  if words == 'end':
+   return 0
+  else:
+      print(s)
+duplicate()
